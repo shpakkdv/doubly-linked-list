0ec8372e2042671c32c26becd2c607d5126d35cc
shpakkdv <shpak_kdv@mail.ru>
2017-01-26 19:55:39 +0300

    1.0

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,15 +1,6 @@
-/**/const Node = require('./node');/**/
-/*
-class Node {
-    constructor(data = null, prev = null, next = null) {
-        this.data = data;
-        this.prev = prev;
-        this.next = next;
-    }
-}*/
+const Node = require('./node');
 
-
-    class LinkedList {
+class LinkedList {
 
     constructor() {
         this._head = null;
@@ -17,7 +8,6 @@ class Node {
     }
 
     get length() {
-        
         if (this._head === null && this._tail === null) {
             return 0;
            
@@ -28,13 +18,19 @@ class Node {
                 count++;
                 temp = temp.next;
             }
-
             return count;
         }
-    };
+    }
+
+    returnNode(index) {
+        var temp = this._head;
+        for (var i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
 
     append(data) {
-
         if (this.isEmpty()) {
             this._head = new Node(data);
             this._tail = this._head;
@@ -43,44 +39,31 @@ class Node {
             this._tail.next = new Node(data, this._tail);
             this._tail = this._tail.next;
         }
-
         return this;
     }
 
     head() {
-
         if (this._head === null) {
             return null;
         }
-
         return this._head.data;
     }
-    
-    tail() {
 
+    tail() {
         if (this._tail === null) {
             return null;
         }
-
         return this._tail.data;
     }
-    
+
     at(index) {
-
-        var temp = this._head;
-
-        for (var i = 0; i < index; i++) {
-            temp = temp.next;
-        }
-
-        return temp.data;
+        return this.returnNode(index).data;
     }
-    // MANY this.length
+
     insertAt(index, data) {
+        var n = this.length;
 
-        var temp = this._head;
-
-        if (index < 0 || index > this.length) {
+        if (index > n || index < 0) {
             return this;
         }
 
@@ -97,118 +80,82 @@ class Node {
             this._head.prev = new Node(data, null, this._head);
             this._head = this._head.prev;
 
-        } else if (index === this.length) {
+        } else if (index === n) {
             this._tail.next = new Node(data, this._tail, null);
             this._tail = this._tail.next;
 
         } else {
-
-            for (var i = 0; i < index; i++) {
-                temp = temp.next;
-            }
-
+            var temp = this.returnNode(index);
             temp.prev = new Node(data, temp.prev, temp);
             temp.prev.prev.next = temp.prev;
-
         }
-
         return this;
     }
-    
+
     isEmpty() {
-
-        if (this.length === 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.length === 0;
     }
-    
-    clear() {
 
+    clear() {
         this._head = null;
         this._tail = null;
-
         return this;
     }
-    
-    //NOT ALL   if tail or head
+
     deleteAt(index) {
+        var n = this.length;
 
-        var temp = this._head;
-
-        if (this.length === 1) {
+        if (n === 1) {
             this.clear();
             return this;
         }
 
-        for (var i = 0; i < index; i++) {
-            temp = temp.next;
+        if (index === 0) {
+            this._head.next.prev = null;
+            this._head = this._head.next;
+            return this;
         }
 
+        if (index === n - 1) {
+            this._tail.prev.next = null;
+            this._tail = this._tail.prev;
+            return this;
+        }
+
+        var temp = this.returnNode(index); 
         temp.prev.next = temp.next;
         temp.next.prev = temp.prev;
-
         return this;
     }
-    //delete element
 
-    //NOT ALL
     reverse() {
-
         var temp = this._tail, buf;
 
         while (temp !== null) {
             buf = temp.prev;
             temp.prev = temp.next;
             temp.next = buf;
-
             temp = temp.next;
         }
 
         temp = this._head;
         this._head = this._tail;
         this._tail = temp;
-
         return this;
     }
-    //перевернуть
 
     indexOf(data) {
-
         var temp = this._head, count = 0;
 
         while (temp !== null) {
-            
             if (temp.data === data) {
                 return count;
             }
-            
             count++;
             temp = temp.next;
         }
-
         return -1;
     }
-    //return index or -1
 }
 
-/**/module.exports = LinkedList;/**/
-
-/*
-var list = new LinkedList();
-
-list.append(1);
-list.append(123);
-list.append(444);
-
-console.log(list.length);
-console.log(list.head());
-console.log(list.tail());
-
-console.log(list.at(0));
-console.log(list.at(1));
-console.log(list.at(2));
-
-list.append(4).reverse().deleteAt(0).clear().insertAt(0, 3);
-*/
+module.exports = LinkedList;
